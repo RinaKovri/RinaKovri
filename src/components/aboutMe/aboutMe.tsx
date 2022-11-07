2058d45d0799c93d09060b756f2e3770a389d52d
Ekaterina Kovrizhina <ekaterina.romanna@gmail.com>
2022-11-07 14:51:13 +0200

    added img

--- src/components/aboutMe/aboutMe.tsx
+++ src/components/aboutMe/aboutMe.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './aboutMe.module.scss'
+import Dandelion_white from './dandelion_white.png';
+
 
 type AboutMeProps = {
 
@@ -8,12 +10,16 @@ type AboutMeProps = {
 const AboutMe: React.FC<AboutMeProps> = () => {
     return (
         <div className={styles.container}>
+            <div className={styles.body}>
             <div className={styles.anim}>
-            <h1><span>Welcome to my page!</span></h1>
+                <h1><span>Welcome to my page!</span></h1>
             </div>
             <h2>My name is Ekaterina Kovrizhina.</h2>
             <h3>I am a studying programming at Varia.</h3>
             <h3>Here you can find my CV and some of my projects.</h3>
+            </div>
+            <button className={styles.animation}>
+            </button>
         </div>
     )
 }
