748912077c223e5cbb6a7801c926e0e50eb6b946
Ekaterina Kovrizhina <ekaterina.romanna@gmail.com>
2023-12-12 14:14:35 +0200

    added info to the AboutMe page

--- src/components/sites/aboutMe.tsx
+++ src/components/sites/aboutMe.tsx
@@ -15,8 +15,12 @@ const AboutMe: React.FC<AboutMeProps> = () => {
         <h2 className={styles.header2}>Thank you for visiting this small website.</h2>
         </div>
         <p className={styles.text2}>I have started my career as a secretary at 20. At 25, I found myself in <i>HR</i> at an IT company. Since then, my interest in programming started growing.<br></br>In 2019 my family moved to Finland. A new page of my life has started: the <i>Finnish language</i> and <i>programming</i>.</p>
-        <p className={styles.text1}>I have learned Finnish in 10 months from A0 to B1 and found a professional school where I could learn programming in finnish. So, I started my education in 2021 and it is still ongoing.<br></br> I have started learning programming at school with game development in <strong>C#</strong>. Then I had a small <strong>Python</strong> course. I also learned <strong>HTML</strong>, <strong>CSS</strong>, <strong>Typescript</strong> and practiced it.
+        <p className={styles.text1}>I have learned Finnish in 10 months from A0 to B1 and found a professional school where I could learn programming in finnish. So, I started my education in 2021 and it is still ongoing.<br></br> I have started learning programming at school with game development using <strong>C#</strong>. I wrote a small programms using <strong>Python</strong>. Also I learned <strong>HTML</strong>, <strong>CSS</strong>, <strong>Typescript</strong>, <strong>Javascript</strong> and <strong>React</strong>. I was learned how to use <strong>MySQL</strong> and <strong>Mongo</strong> databases.
         </p>
+        <p className={styles.text1}>During the practice time in school I was working in a team on the creation of a web interface for an application for booking school rooms and involved to the start-up project. Our task was to build the system for electric charger point. I was leading the backend team, we have successfully started to write <strong>OCPP</strong> and built all the backend for the website and application.
+        </p>
+        <p className={styles.text1}>Also I helped to build the website in <strong>Javascript</strong> from the beginnig. During this task I was learning <strong>Bootstrap.</strong></p>
+        <p className={styles.text1}>Currently I continue to study programming at the Metropolia University of Applied Sciences.</p>
         <h4 className={styles.header4}>If you would like to contact me any time with any question, please, do not hesitate!</h4><br></br><p className={styles.footer}>You can find my Linkedin page link and email link below.</p>
         <p className={styles.footer}>Thank you for your time!<br></br>Sincerely yours,<br></br>Ekaterina</p>
       </div>
--- src/components/sites/cv.tsx
+++ src/components/sites/cv.tsx
@@ -11,42 +11,42 @@ const Cv: React.FC<CvProps> = () => {
   return (
     <div className={styles.container}>
       <ul className={styles.cvBody}>
-      <li className={styles.leftSide}>
-        <h2 className={styles.h2left}>Skills</h2>
-        <ul>
-          {
-            mySkills && mySkills.map(x => <li key={x.key} className={styles.leftItems}>{x.skill}</li>)
-          }
-        </ul>
-        <h2 className={styles.h2left}>Languages</h2>
-        <ul>
-          {
-            myLanguages && myLanguages.map(x => <li key={x.key} className={styles.leftItems}>{x.skill}</li>)
-          }
-        </ul>
-        <h2 className={styles.h2left}>Education</h2>
-        <ul>
-          <li className={styles.leftItems}>Tieto- ja viestintätekniikka,  perustutkinto</li>
-          <li className={styles.leftdate}>Varia ammattiopisto, 2021-now</li>
-          <li className={styles.leftItems}>Human Resources, Bachelor degree</li>
-          <li className={styles.leftdate}>Sinergy Univercity, 2016-2019</li>
-        </ul>
-      </li>
-      <li className={styles.rightSide}>
-        <h2 className={styles.h2right}>Experience</h2>
-        <div className={styles.rightList}>
-          <h3 className={styles.rightItems}>Student</h3>
-          <h5 className={styles.rightdate}>Varia ammattiopisto, 2021-now</h5>
-          <p className={styles.rightP}>Created my own game in Unity using C#.<br></br> Wrote a simple calculator in Python.<br></br> Worked in a team on the creation of a web interface for an application for booking school rooms.<br></br> Studied Javascript, Typescript, HTML and CSS courses at the Code Academy</p>
-          <h3 className={styles.rightItems}>HR-manager, internship</h3>
-          <h5 className={styles.rightdate}>LOGRUS ry, Finland, 2020-2021</h5>
-          <p className={styles.rightP}>My duties included helping the CEO, communication between participants, searching for volunteers, advertising</p>
-          <h3 className={styles.rightItems}>HR-manager</h3>
-          <h5 className={styles.rightdate}>Sidenis LLC, Russia, 2013-2019</h5>
-          <p className={styles.rightP}>Was engaged in search and hiring of IT employees. Managed the company's paperwork.</p>
-        </div>
-      </li>
-    </ul>
+        <li className={styles.leftSide}>
+          <h2 className={styles.h2left}>Skills</h2>
+          <ul>
+            {
+              mySkills && mySkills.map(x => <li key={x.key} className={styles.leftItems}>{x.skill}</li>)
+            }
+          </ul>
+          <h2 className={styles.h2left}>Languages</h2>
+          <ul>
+            {
+              myLanguages && myLanguages.map(x => <li key={x.key} className={styles.leftItems}>{x.skill}</li>)
+            }
+          </ul>
+          <h2 className={styles.h2left}>Education</h2>
+          <ul>
+              <li className={styles.leftItems}>Tieto- ja viestintätekniikka,  perustutkinto</li>
+              <li className={styles.leftdate}>Varia ammattiopisto, 2021-2023</li>
+              <li className={styles.leftItems}>Human Resources, Bachelor degree</li>
+              <li className={styles.leftdate}>Sinergy Univercity, 2016-2019</li>
+          </ul>
+        </li>
+        <li className={styles.rightSide}>
+          <h2 className={styles.h2right}>Experience</h2>
+          <div className={styles.rightList}>
+            <h3 className={styles.rightItems}>Junior software developer, internship</h3>
+            <h5 className={styles.rightdate}>Lokkit Oy, 06.2023-12.2023</h5>
+            <p className={styles.rightP}>Helped to build the electric charger point system using Javascript, OCPP <br></br> Took a part in creating website frontend<br></br></p>
+            <h3 className={styles.rightItems}>HR-manager, internship</h3>
+            <h5 className={styles.rightdate}>LOGRUS ry, Finland, 12.2020-03.2021</h5>
+            <p className={styles.rightP}>My duties included helping the CEO, communication between participants, searching for volunteers, advertising</p>
+            <h3 className={styles.rightItems}>HR-manager</h3>
+            <h5 className={styles.rightdate}>Sidenis LLC, Russia, 2013-2019</h5>
+            <p className={styles.rightP}>Was engaged in search and hiring of IT employees. Managed the company's paperwork.</p>
+          </div>
+        </li>
+      </ul>
     </div >
   )
 }
